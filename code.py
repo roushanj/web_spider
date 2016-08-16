7014524073a2d8a2305a51799fd1df0ca99d20a5
rjupes <roushan.jha0709@gmail.com>
2016-08-16 12:48:32 +0530

    made changes

--- code.py
+++ code.py
@@ -11,4 +11,23 @@ def trade_spider(max_pages):
 		source_code = request.get(url)
 		plain_text = source_code.txt 
 		Soup = BeautifulSoup(plain_text)
-		 
+		for link in Soup.findAll('a',{'class:'item name'}):
+			href = "www.main_url.com" + link.get('href')
+			title = link.string 
+
+
+			get_single_item_data(href)
+			page +=1
+
+def get_single_item_data(item_url):
+	
+	source_code = request.get(item_url)
+	plain_text = source_code.txt
+	Soup = BeautifulSoup(plain_text)
+	for item_name in Soup.find('div',{'class':'i-name'}):
+		print(item_name).string
+
+
+trade_spider(4)
+
+		 	 
